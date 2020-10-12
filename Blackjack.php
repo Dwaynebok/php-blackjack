2efdf3929b6cefbf1a843e5ad61ffb6dfbf4763d
Dwaynebok <dwaynebok19@gmail.com>
2020-10-12 15:26:28 +0200

    created  public functions and instantiated new player to player and dealer

--- Blackjack.php
+++ Blackjack.php
@@ -7,7 +7,26 @@ class Blackjack
     private $dealer;
     private $deck;
 
+    /**
+     * @return mixed
+     */
+    public function getDealer()
+    {
+        return $this->dealer;
+    }
 
+    /**
+     * @return mixed
+     */
+    public function getPlayer()
+    {
+        return $this->player;
+    }
 
+    public function __construct() // instantiate Instantiate the Player class twice, insert it into the player property and a dealer property.
+    {
+        $player = new Player();
+        $dealer = new Player();
+    }
 
 }
